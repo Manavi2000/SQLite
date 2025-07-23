284f2381557a76470cf51dafc14c4c89909e7ccb
Manavi-Yadav515 <Manavi.Yadav515@ibm.com>
2025-07-24 03:24:43 +0530

    Format, primary key and Where caluse

--- database.py
+++ database.py
@@ -46,11 +46,48 @@ cursor.execute("""SELECT * FROM customers""")
 #cursor.fectchall() : will fect all the information but to see on the bacsh you need to print it. 
 #cursor.fectone(): will fetch the first one only.
 #cursor.fectmany(3) : will fect number of input we mentioned. 
-print(cursor.fetchall()) # return the python list [(),()] in this form. 
+#print(cursor.fetchall()) # return the python list [(),()] in this form. 
+# print(cursor.fetchone()[0])
+# print(cursor.fetchone()[1])
+# print(cursor.fetchone()[2])
 
+items = cursor.fetchall()
+for item in items: 
+    print(item)
+    
+# each will be printed as tuple 
+# way to format the output of the databse. 
+# ('Jhon', 'Elder', 'john@gmail.com')
+# ('Tim', 'Old', 'TIMAn@gmail.com')
+# ('Mary', 'Brown', 'Mary@gmail.com')
+# ('Was', 'Yadav', 'a@ibm.com')
+# ('Text', 'Kim', 'kim@inm.com')
+# ('OM', 'abv', 'abv@om.com')
+print("NAME "+"\t\t"+"EMAIL")
+for item in items: 
+    print(item[0] + "\t\t"+item[2])
+    
+    
+#primmary id : unique value in the table. 
+# So sqlite create this id on it own. we don;t have to create it 
+# to see it print the rowid, if we specify that we don't want primary key then it will not create it own
+# but majorly we want it only
+cursor.execute("""SELECT rowid,* FROM customers""")
+print(cursor.fetchall())
 print('Command excuted succesfully')
 # it will commit our changes, 
 con.commit()
+
+#logical oprator can also be used here.  =, >= , <= 
+cursor.execute("""SELECT * FROM customers WHERE first_name = 'Jhon'
+               """)
+print(cursor.fetchall())
+cursor.execute("""SELECT * FROM customers WHERE last_name LIKE 'ki%'
+               """)
+print(cursor.fetchall())
+cursor.execute("""SELECT * FROM customers WHERE email LIKE '%.com'
+               """)
+print(cursor.fetchall())
 
 #everytime we create a connection we try to cclose it 
 con.close()
